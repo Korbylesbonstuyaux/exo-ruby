35e4eb8f7aa66aa93db816df515c8ca8bae3682d
Korbylesbonstuyaux <korb.softwares@gmail.com>
2015-12-26 22:24:05 +0100

    Update first and second parts

--- main.rb
+++ main.rb
@@ -1,21 +1,25 @@
-# PART 3
-# ==== Partie "Introduction à Ruby"
+# EXERCICES OF RUBY
 
-#Fct Surface of a triangle : bas eand height
+#Fct Surface of a triangle
 def surface(base, height)
-  puts (base * height) / 2
+  if ((base.is_a? Float)  && (base.is_a? Float))
+    if ((base >= 0) && (base >= 0))
+      puts "La surface est de : #{(base * height) / 2}."
+    else puts "Un ou des paramètres envoyés ne sont pas positifs."
+    end
+  else puts "Un ou des paramètres envoyés ne sont pas des floats."
+  end
 end
 
-#Fct Price TTC with a TVA
+#Fct Price TTC
 def priceTTC(price_ht_unit, tva_rate, quantity)
-  puts "Prix total en TTC :  #{(price_ht_unit + (price_ht_unit * tva_rate / 100)) * quantity} €"
-end
-
-#Fct Swap
-def swap(a, b)
-  c = a
-  a = b
-  b = c
+  if ((price_ht_unit.is_a? Float)  && (tva_rate.is_a? Float) && (quantity.is_a? Float))
+    if ((price_ht_unit >= 0)  && (tva_rate >= 0) && (quantity >= 0))
+      puts "Prix total en TTC :  #{(price_ht_unit + (price_ht_unit * tva_rate / 100)) * quantity} €."
+    else puts "Un ou des paramètres envoyés ne sont pas positifs."
+    end
+  else puts "Un ou des paramètres envoyés ne sont pas des floats."
+  end
 end
 
 #Fct Average with weighting
@@ -30,10 +34,362 @@ def average(note_array, weighting_array)
     end
 
     per_cent = (note_som / weig_som) * 100 / 20
-    puts "Average : #{per_cent} %"
-  else puts "Error"
+    puts "La moyenne est de #{per_cent} %."
+  else puts "Le nombre de notes n'est pas égal au nombre de pondération."
   end
+end
 
+#Fct Sum of digits of number with char function
+def sumWithFctChar(string)
+  sum = 0
+  string.each_char do
+    |chr|
+    sum += chr.to_i
+  end
+  puts "La somme des chiffres de \"#{string}\" est égal à #{sum}"
+end
+
+#Fct maximum of 2 numbers
+def max2nb(nb1, nb2)
+  if((nb1.is_a? Float) && (nb2.is_a? Float))
+    if(nb1 != nb2)
+      if (nb1 > nb2)
+        puts "Le maximum entre ces 2 nombres #{nb1} et #{nb2} est #{nb1}."
+      else puts "Le maximum entre ces 2 nombres #{nb1} et #{nb2} est #{nb2}."
+      end
+    else puts "Le maximum entre ces 2 nombres #{nb1} et #{nb2} est #{nb2}."
+    end
+  else puts "Un ou des paramètres ne sont pas de type float."
+  end
+end
+
+#Fct maximum of 3 numbers
+def max3nb(nb1, nb2, nb3)
+  if((nb1.is_a? Float) && (nb2.is_a? Float) && (nb3.is_a? Float))
+    if(nb1 == nb2 && nb1 == nb3)
+      puts "Le maximum entre ces 3 nombres #{nb1}, #{nb2} et #{nb3} est #{nb3}."
+    else if (nb1 <= nb2)
+           if(nb2 <= nb3)
+             puts "Le maximum entre ces 3 nombres #{nb1}, #{nb2} et #{nb3} est #{nb3}."
+           else puts "Le maximum entre ces 3 nombres #{nb1}, #{nb2} et #{nb3} est #{nb2}."
+           end
+         else if(nb1 <= nb3)
+                puts "Le maximum entre ces 3 nombres #{nb1}, #{nb2} et #{nb3} est #{nb3}."
+              else puts "Le maximum entre ces 3 nombres #{nb1}, #{nb2} et #{nb3} est #{nb1}."
+              end
+       end
+    end
+  else puts "Un ou des paramètres ne sont pas de type float."
+  end
+end
+
+#Fct sign
+def sign(nb)
+  if(nb.is_a? Float)
+    if(nb == 0)
+      puts "Le nombre #{nb} est nul"
+    else if(nb < 0)
+           puts "Le nombre #{nb} est négatif."
+         else puts "Le nombre #{nb} est positif."
+         end
+    end
+  else puts "Un ou des paramètres ne sont pas de type float."
+  end
+end
+
+#Fct include
+def isInclude(nb1, nb2, nb3, borned)
+  if((nb1.is_a? Float) && (nb2.is_a? Float) && (nb3.is_a? Float))
+    if(!borned)
+      if( ((nb1 > nb2) && (nb1 < nb3)) || ((nb1 < nb2) && (nb1 > nb3)) )
+        puts "Le nombre #{nb1} est bien compris #{nb2} et #{nb3}."
+      else puts "Le nombre #{nb1} N'est PAS compris #{nb2} et #{nb3}."
+      end
+    else if( ((nb1 >= nb2) && (nb1 <= nb3)) || ((nb1 <= nb2) && (nb1 >= nb3)))
+           puts "Le nombre #{nb1} est bien compris #{nb2} et #{nb3} (bornes comprises)."
+         else puts "Le nombre #{nb1} N'est PAS compris #{nb2} et #{nb3} (bornes comprises)."
+         end
+    end
+  else puts "Un ou des paramètres ne sont pas de type float."
+  end
+end
+
+#Fct nomber of days by month
+def daysByMonth(num)
+  if(num.is_a? Integer)
+    case num
+    when 2
+      puts "Ce mois comporte 28 jours."
+    when 4, 6, 9, 11
+      puts "Ce mois comporte 30 jours."
+    when 1, 3, 5, 7, 8, 10, 12
+      puts "Ce mois comporte 31 jours."
+    else puts "Ce mois n'existe pas. Le chiffre doit être entre 1 et 12."
+    end
+  else puts "Ce n'est pas un Integer."
+  end
+end
+
+#Fct is a bissextille year
+def bissextille(year)
+  if(year.is_a? Integer)
+    if( (year % 400 == 0) || ((year % 100 != 0) && (year % 4 == 0)) )
+      puts "L'Année #{year} bissextile."
+    else puts "L'Annnée #{year} est NON bissextile."
+    end
+  else "L'Année n'est pas un entier."
+  end
+end
+
+#Fct is a valid date : day, month and year (4int)
+def isValidDate(day, month, year)
+  if((day.is_a? Integer) && (month.is_a? Integer) && (year.is_a? Integer))
+    max_day = 31
+    if((month >= 1) && (month <= 12))
+      case month
+      when 2
+        max_day = 28
+      when 4, 6, 9, 11
+        max_day = 30
+      end
+
+      if((day >= 1) && (day <= max_day))
+        puts "La date est valide."
+      else puts "Le jour de la date n'est pas correct avec le mois."
+      end
+
+    else puts "Le mois de la date n'est pas correct."
+    end
+  else puts "La date n'est pas correcte."
+  end
+end
+
+#Fct correct place on alternative parking
+def correctAlternativeParking(day, num)
+  if((day.is_a? Integer) && (num.is_a? Integer) && (day >= 1 && day <= 31))
+    if((day >= 1) && (day <=15))
+      puts num % 2 == 1 ?  "Vous êtes bien garés" : "Vous N'êtes PAS bien garé."
+    else puts num % 2 == 0 ? "Vous êtes bien garés" : "Vous N'êtes PAS bien garé."
+    end
+  else puts "Le jour et/ou le numéro ne sont pas un entier. Le jour doit être compris entre 1 et 31."
+  end
+end
+
+puts "Liste des programmes à tester :"
+puts "1: Calcule la surface d'un triangle."
+puts "2: Prix TTC en fonction du prix HT, la TVA en % et la quantité souhaitée."
+puts "3: Permutation de valeurs de 2 variables SANS UTILISER DE FONCTION."
+puts "4: Note moyenne en pourcentage avec en paramètres 3 notes sur 20 et des pondération pour chaque note."
+puts "5: Somme des chiffres en utilisant les fonctions sur le type String."
+puts "6: Le maximum de 2 nombres donnés."
+puts "7: Le maximum de 3 nombres donnés."
+puts "8: Indique le signe d'un nombre."
+puts "9: Indique si le premier nombre est compris entre les deux autres."
+puts "10: Nombre de jours dans un mois."
+puts "11: Indique si une année est bissextile ou pas."
+puts "12: Indique si une date est valide."
+puts "13: Le stationnement alternatif - Indique si vous êtes bien stationné
+(du 1 au 15 du mois, on se gare du côté des maisons ayant un numéro impair, et le reste du mois, du côté pair)"
+puts "Entrez s'il vous plaît le numéro associé au programme souhaité."
+choice = gets.chomp.to_i
+puts "========================================================================="
+
+case choice
+when 1
+  puts "Programme choisi : Calcule la surface d'un triangle."
+  print "Veuillez entrer une valeur pour la base : "
+  base = gets.chomp.to_f
+  print "Veuillez entrer une valeur pour la hauteur : "
+  height = gets.chomp.to_f
+  surface(base, height)
+
+when 2
+  puts "Programme choisi : Prix TTC en fonction du prix HT, la TVA et la quantité souhaitée."
+  print "Veuillez entrer une valeur pour le prix TTC : "
+  price_ht_unit = gets.chomp.to_f
+  print "Veuillez entrer une valeur pour la TVA : "
+  tva_rate = gets.chomp.to_f
+  print "Veuillez entrer un quantité : "
+  quantity = gets.chomp.to_f
+  priceTTC(price_ht_unit, tva_rate, quantity)
+
+when 3
+  puts "Programme choisi : Permutation de valeurs de 2 variables SANS UTILISER DE FONCTION."
+  print "Veuillez entrer une valeur pour la variable 1 :"
+  var1 = gets.chomp
+  print "Veuillez entrer une valeur pour la varaible 2 :"
+  var2 = gets.chomp
+  puts "AVANT la permutation : la variable 1 = #{var1} && la variable 2 = #{var2}."
+  var3 = var1
+  var1 = var2
+  var2 = var3
+  puts "APRES la permutation : la variable 1 = #{var1} && la variable 2 = #{var2}."
+
+when 4
+  puts "Programme choisi : Note moyenne en pourcentage."
+  puts "Veuillez entrer 3 notes sur 20."
+  notes = Array.new
+  nb = 0
+  loop do
+    print "Valeur de la note #{nb+1} : "
+    val = gets.chomp.to_f
+    if (val >= 0 && val <= 20)
+      notes.push(val)
+      nb += 1
+    end
+    break if nb >= 3
+  end
+  puts "Veuillez entrer les pondérations associées."
+  weights = Array.new
+  nb = 0
+  loop do
+    print "Valeur de la pondération pour la note #{nb+1} : "
+    val = gets.chomp.to_f
+    if (val >= 0)
+      weights.push(val)
+      nb += 1
+    end
+    break if nb >= 3
+  end
+  average(notes, weights)
+
+when 5
+  puts "Programme choisi : Somme des chiffres."
+  puts "Veuillez entrer des chiffres (les autres caractères seront considérés comme des 0)."
+  string = gets.chomp.gsub(/[^\d]/, '')
+  sumWithFctChar(string)
+
+when 6
+  puts "Programme choisi : Le maximum de 2 nombres donnés."
+  nombres = Array.new
+  nb = 0
+  loop do
+    print "Choississez le nombre #{nb+1} : "
+    val = gets.chomp.to_f
+    nombres.push(val)
+    nb += 1
+    break if nb >= 2
+  end
+  max2nb(nombres[0], nombres[1])
+
+when 7
+  puts "Programme choisi : Le maximum de 3 nombres donnés."
+  nombres = Array.new
+  nb = 0
+  loop do
+    print "Choississez le nombre #{nb+1} : "
+    val = gets.chomp.to_f
+    nombres.push(val)
+    nb += 1
+    break if nb >= 3
+  end
+  max3nb(nombres[0], nombres[1], nombres[2])
+
+when 8
+  puts "Programme choisi : Indique le signe d'un nombre."
+  puts "Veuillez entrer un nombre."
+  nombre = gets.chomp.to_f
+  sign(nombre)
+
+when 9
+  puts "Programme choisi : Indique si le premier nombre est compris entre les deux autres."
+  nombres = Array.new
+  nb = 0
+  loop do
+    print "Choississez le nombre #{nb+1} : "
+    val = gets.chomp.to_f
+    nombres.push(val)
+    nb += 1
+    break if nb >= 3
+  end
+  borned = false
+  puts "Activer les bornes : (Tapez \"oui\" sinon rien)"
+  resp = gets.chomp
+  if (resp === "oui")
+    borned = true
+  end
+  puts borned
+  isInclude(nombres[0], nombres[1], nombres[2], borned)
+
+when 10
+  puts "Programme choisi : Nombre de jours dans un mois."
+  puts "Le chiffre doit être compris entre 1 et 12 (1 pour Janvier et 12 pour Décembre)."
+  month = 0
+  loop do
+    print "Saississez votre mois : "
+    month = gets.chomp.to_i
+    if (month < 1 || month > 12)
+      puts "Ce nombre est invalide."
+    end
+    break if (month >= 1 && month <= 12)
+  end
+  daysByMonth(month)
+
+when 11
+  puts "Programme choisi : Indique si une année est bissextile ou pas."
+  print "Veuillez entrer une année : "
+  year = gets.chomp.to_i
+  bissextille(year)
+
+when 12
+  puts "Programme choisi : Indique si une date est valide."
+  puts "Le jour doit être compris entre 1 et 31."
+  day = 0
+  loop do
+    print "Saississez votre jour : "
+    day = gets.chomp.to_i
+    if (day < 1 || day > 31)
+      puts "Ce nombre est invalide."
+    end
+    break if (day >= 1 && day <= 31)
+  end
+  puts "Le mois doit être compris entre 1 et 12."
+  month = 0
+  loop do
+    print "Saississez votre mois : "
+    month = gets.chomp.to_i
+    if (month < 1 || month > 12)
+      puts "Ce nombre est invalide."
+    end
+    break if (month >= 1 && month <= 12)
+  end
+  print "Saississez votre année : "
+  year = gets.chomp.to_i
+  isValidDate(day, month, year)
+
+when 13
+  puts "Programme choisi : Le stationnement alternatif - Indique si vous êtes bien stationné."
+  puts "Le jour doit être compris entre 1 et 31."
+  day = 0
+  loop do
+    print "Saississez votre jour : "
+    day = gets.chomp.to_i
+    if (day < 1 || day > 31)
+      puts "Ce nombre est invalide."
+    end
+    break if (day >= 1 && day <= 31)
+  end
+  num = 0
+  loop do
+    print "Saississez votre numéro de rue : "
+    num = gets.chomp.to_i
+    if (num <= 0 )
+      puts "Ce nombre est invalide."
+    end
+    break if (num > 0)
+  end
+  correctAlternativeParking(day, num)
+
+else puts "Ce programme n'existe pas dans la liste."
+
+end
+
+
+#Fct Swap
+def swap(a, b)
+  c = a
+  a = b
+  b = c
 end
 
 #Fct Sum of digits of number
@@ -48,155 +404,6 @@ def sum(nb)
       puts "Somme des chiffres du nombre : #{som}"
   end
 end
-
-# surface(3, 5)
-
-# priceTTC(100.5, 10, 2)
-
-# c = 5
-# d = 6
-# swap(c, d)
-# puts "c = #{c}"
-
-# n_array = [10, 10, 12]
-# w_array = [1, 2, 3]
-# average(n_array, w_array)
-
-# sum(987)
-
-# ==== Partie "Les structures de contrôle"
-
-#Fct maximum of 2 numbers
-def max2nb(nb1, nb2)
-  if(nb1 != nb2)
-    if (nb1 > nb2)
-      puts "MAX : #{nb1}"
-    else puts "MAX : #{nb2}"
-    end
-  else puts "MAX : #{nb2}"
-  end
-end
-
-#Fct maximum of 3 numbers
-def max3nb(nb1, nb2, nb3)
-  if(nb1 == nb2 && nb1 == nb3)
-    puts "MAX : #{nb3}"
-  else if (nb1 <= nb2)
-         if(nb2 <= nb3)
-           puts "MAX : #{nb3}"
-         else puts "MAX : #{nb2}"
-         end
-       else if(nb1 <= nb3)
-              puts "MAX : #{nb3}"
-            else puts "MAX : #{nb1}"
-            end
-     end
-  end
-end
-
-#Fct sign
-def sign(nb)
-    if(nb == 0)
-      puts "NUL"
-    else if(nb < 0)
-           puts "INF"
-         else puts "SUP"
-         end
-    end
-end
-
-#Fct include
-def isInclude(nb1, nb2, nb3, borned)
-  if(borned)
-    if( ((nb1 > nb2) && (nb1 < nb3)) || ((nb1 < nb2) && (nb1 > nb3)) )
-      puts "Le nombre 1 est bien compris"
-    else puts "Le nombre 1 N'est PAS bien compris"
-    end
-  else if( ((nb1 >= nb2) && (nb1 <= nb3)) || ((nb1 <= nb2) && (nb1 >= nb3)))
-         puts "Le nombre 1 est bien compris (bornes comprises)"
-       else puts "Le nombre 1 N'est PAS bien compris (bornes comprises)"
-       end
-  end
-end
-
-#Fct nomber of days by month
-def daysByMonth(num)
-  if(num.is_a? Integer)
-    case num
-    when 2
-      puts "Ce mois comporte 28 jours"
-    when 4, 6, 9, 11
-      puts "Ce mois comporte 30 jours"
-    when 1, 3, 5, 7, 8, 10, 12
-      puts "Ce mois comporte 31 jours"
-    else puts "Ce mois n'existe pas"
-    end
-  else puts "Ce n'est pas un chiffre compris entre 1 et 12"
-  end
-end
-
-#Fct is a bissextille year
-def bissextilleYear(year)
-  if(year.is_a? Integer)
-    if( (year % 400 == 0) || ((year % 100 != 0) && (year % 4 == 0)) )
-      puts "Année bissextile"
-    else puts "Annnée NON bissextile"
-    end
-  else "Ce n'est pas un entier"
-  end
-end
-
-#Fct is a valid date : day, month and year (4int)
-def isValidDate(day, month, year)
-  if((day.is_a? Integer) && (month.is_a? Integer) && (year.is_a? Integer))
-
-    max_day = 31
-    error = false
-
-    case month
-    when 2
-      max_day = 28
-    when 4, 6, 9, 11
-      max_day = 30
-    else error = true
-    end
-
-    if(!error && (day >= 1) && (day <= max_day))
-      puts "La date est valide"
-    else puts "La date n'est pas valide. Erreur dans dans le mois ou le jour"
-    end
-
-  else puts "Date n'est pas du type day (00), month (00) and year (0000). Ex : 1/2/1980"
-  end
-end
-
-#Fct correct place on alternative parking
-def correctAlternativeParking(day, num)
-  if ((day.is_a? Integer) && (num.is_a? Integer) && (day >= 1 && day <= 31))
-    if day === 1..15
-      puts day % 2 == 1 ?  "Vous êtes bien garés" : "Vous N'êtes PAS bien garé"
-    else puts day % 2 == 0 ? "Vous êtes bien garés" : "Vous N'êtes PAS bien garé"
-    end
-  else puts "Le jour et/ou le numéro ne sont pas un entier. Le jour doit être compris entre 1 et 31."
-  end
-end
-
-# max2nb(2,4)
-
-# max3nb(8,7,6)
-
-# sign(-6)
-
-# isInclude(2,7,0, false)
-
-# daysByMonth(3)
-
-# bissextilleYear(2012)
-
-# isValidDate(9, 2, 1940)
-
-# correctAlternativeParking(3, 3)
-
 
 # ==== partie "Les boucles avec Ruby"
 
